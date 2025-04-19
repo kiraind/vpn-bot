2cb64e102a0bc0d9bbd50c6de3806fdff628739d
Alexei Paksevatkin <me@paksevatkin.ru>
2025-04-19 09:34:06 +0300

    retries to check membership

--- src/checkMembership.ts
+++ src/checkMembership.ts
@@ -1,17 +1,34 @@
 import { tgChatId } from './config'
 import { bot } from './bot'
 
+const retries = 5
+
 export async function checkMembership(userId: number): Promise<[boolean, string]> {
-  const member = await bot.getChatMember(tgChatId, userId).catch(() => ({
-    status: 'unknown'
-  }))
+  let status = 'unknown'
+  let reason = 'unknown'
+
+  for(let i = 0; i < retries; i += 1) {
+    try {
+      const member = await bot.getChatMember(tgChatId, userId)
+
+      status = member.status
+      reason = 'ok'
+      break
+    } catch(e) {
+      reason = JSON.stringify(e, Object.getOwnPropertyNames(e))
+
+      if(i !== retries - 1) {
+        await new Promise((res) => setTimeout(res, 5000))
+      }
+    }
+  }
 
   return [
     (
-      member.status === 'administrator' ||
-      member.status === 'creator' ||
-      member.status === 'member'
+      status === 'administrator' ||
+      status === 'creator' ||
+      status === 'member'
     ),
-    member.status
+    reason
   ]
 }
